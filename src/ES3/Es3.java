4a9b349c57dadc2e34038c12265e0be8a1472278
OleksandrLarionov <larionov.alex23@gmail.com>
2023-12-05 16:51:56 +0100

    es3

---
+++ src/ES3/Es3.java
@@ -0,0 +1,20 @@
+package ES3;
+
+import ES3.entities.Rubrica;
+
+public class Es3 {
+    public static void main(String[] args) {
+        Rubrica.addContact("Marco", 321578755);
+        Rubrica.addContact("Franco", 328545855);
+        Rubrica.addContact("Ajeje", 339548555);
+
+        Rubrica.removeContact("Marco");
+
+        Rubrica.findPhoneNumberByName("Ajeje");
+
+        Rubrica.findNameByAPhoneNumber(339548555);
+
+        System.out.println("L'elenco di tutte le persone salvate :" + Rubrica.getRubrica());
+    }
+
+}
---
+++ src/ES3/entities/Rubrica.java
@@ -0,0 +1,49 @@
+package ES3.entities;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Rubrica {
+
+    private static Map<String, Integer> rubrica = new HashMap<>();
+
+    public static void addContact(String name, int phoneNumber) {
+        rubrica.put(name, phoneNumber);
+    }
+
+    public static void removeContact(String name) {
+        if (rubrica.containsKey(name)) {
+            int phoneNumber = rubrica.remove(name);
+        } else {
+            System.out.println("Contatto di nome :" + name + "non trovato");
+        }
+    }
+
+    public static void findPhoneNumberByName(String name) {
+        if (rubrica.containsKey(name)) {
+            int phoneNumber = rubrica.get(name);
+            System.out.println("Il numero della persona cercata di nome " + name + " è :" + phoneNumber);
+        }
+    }
+
+    public static void findNameByAPhoneNumber(int phoneNumber) {
+        for (Map.Entry<String, Integer> entry : rubrica.entrySet()) {
+            if (entry.getValue().equals(phoneNumber)) {
+                System.out.println("Nome : " + entry.getKey() + ", " + phoneNumber);
+                return;
+            }
+        }
+        System.out.println("Nessuna persona trovata con il numero di telefono: " + phoneNumber);
+    }
+
+    public static Map<String, Integer> getRubrica() {
+        return rubrica;
+    }
+
+    @Override
+    public String toString() {
+        return "Rubrica{" +
+                "rubrica=" + rubrica +
+                '}';
+    }
+}
